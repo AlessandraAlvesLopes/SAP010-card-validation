96c9a07a6bc58e28716780b22a329c9ae1fbc579
Alessandra Alves <alessandraalveslopes04@gmail.com>
2023-04-24 14:25:06 -0300

    Implementei Js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import validator from './validator.js';
 
-console.log(validator);
+let btn = document.getElementById("botao");
+btn.addEventListener('click',start);
+
+function start(){
+
+    let numC = document.getElementById('numCard').value;
+    let outside = document.getElementById ('out');
+
+    let res = validator.isValid(numC);
+    let cardBlock = validator.maskify(numC);
+    
+    if (numC === ""){
+        outside.innerHTML = "Insira um valor";
+    }
+
+    else if(res == true){
+        outside.innerHTML = cardBlock + "<h2 style='color: green;'> Cartão válido </h2>";
+    }
+
+    else{
+        outside.innerHTML = cardBlock + "<h2 style= 'color: red;'> Cartão inválido </h2>";
+    }
+
+}
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,38 @@
 const validator = {
-  // ...
-};
+
+   isValid:(card)=>{
+    let nCard = card.split("").reverse();
+    let rescard = 0;
+
+    for(let i = 0; i<nCard.length; i++){
+      let num1Card = parseInt(nCard[i]);
+
+      if (i % 2 !== 0 && nCard >=5)
+      rescard = rescard + (nCard * 2) -9;
+
+      else if (i % 2 !== 0 && nCard < 5)
+      rescard = rescard + (nCard * 2);
+
+      else {
+        rescard = rescard + nCard;
+      }
+    }
+
+    if (rescard % 10 === 0){
+      return true;
+    }
+    else{
+      return false;
+    }
+  },
+
+  maskify: function(card1){
+    return card1.replace( )
+  }
+
+}
 
 export default validator;
+
+
+
